08893e1e77cf7a3ebf6cd08d37fc1bfc903c4544
Mücahid AYDIN <mucahidaydin047@gmail.com>
2024-07-19 17:00:32 +0300

    Update user_create.sh

--- conf/users/user_create.sh
+++ conf/users/user_create.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
+rm -f user.htpasswd
 read -p "Please enter your username: " username
+
+read -sp "Please enter your password: " password
+echo
+
 echo -n "$username:" >> user.htpasswd
-openssl passwd -apr1 >> user.htpasswd
+openssl passwd -apr1 "$password" >> user.htpasswd
+
+echo "User $username has been added to user.htpasswd."
